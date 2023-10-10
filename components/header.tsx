44f13a1255ae1ba6477865da17d62245a4207058
HossamTuwano <hossamtuwano@gmail.com>
2023-10-10 14:14:00 +0300

    style(header): drop down dynamically styled

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,35 @@
-import { Text, View } from "react-native";
+import {
+  View,
+  Animated,
+  Easing,
+  useWindowDimensions,
+  Text,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import Entypo from "react-native-vector-icons/Entypo";
 import SelectDropdown from "react-native-select-dropdown";
+import { PropsWithChildren, useRef, useState } from "react";
+import type { EasingFunction } from "react-native";
 
-const countries = ["Home", "Popular"];
+const values = ["Home", "Popular"];
 
 const Header = () => {
+  // const [isOpen, setIsOpen] = useState(false);
+  // const fadeAnimation = useRef(new Animated.Value(0)).current;
+  const opacity = new Animated.Value(0);
+  const { width } = useWindowDimensions();
+
+  const animatedCaret = (easing: EasingFunction) => {
+    opacity.setValue(0);
+    Animated.timing(opacity, {
+      toValue: 0,
+      easing,
+      duration: 3000,
+      useNativeDriver: true,
+    }).start();
+  };
+
   return (
     <SafeAreaView className="px-3 py-2">
       <View className="flex-row justify-between items-center">
@@ -16,26 +39,55 @@ const Header = () => {
           </View>
           {/* <DropDownPicker /> */}
           <SelectDropdown
-            buttonTextStyle= {{fontWeight: 'bold'}}
-            buttonStyle={{ width:130, borderRadius:8}}
+            defaultValueByIndex={0}
+            onFocus={() => animatedCaret(Easing.ease)}
+            buttonTextStyle={{ fontWeight: "bold" }}
+            buttonStyle={{ width: 130, borderRadius: 8 }}
             defaultButtonText="Home"
-            renderDropdownIcon={() => (
-              <Entypo name="chevron-small-down" size={30} />
-            )}
-            data={countries}
-            onSelect={(selectedItem, index) => {
-              console.log(selectedItem, index);
+            dropdownStyle={{
+              width: width,
+              marginTop: -17,
+              flex: 1,
+              justifyContent: "space-between",
+              flexDirection: "row",
+              marginLeft: -50,
             }}
-            buttonTextAfterSelection={(selectedItem, index) => {
-              // text represented after item is selected
-              // if data array is an array of objects then return selectedItem.property to render after item is selected
-              return selectedItem;
+            renderDropdownIcon={(selectedItem, index) => {
+              return (
+                <Animated.View>
+                  {selectedItem ? (
+                    <Entypo name="chevron-small-up" size={30} color="#000" />
+                  ) : (
+                    <Entypo name="chevron-small-down" size={30} color="#000" />
+                  )}
+                </Animated.View>
+              );
             }}
-            rowTextForSelection={(item, index) => {
-              // text represented for each item in dropdown
-              // if data array is an array of objects then return item.property to represent item in dropdown
-              return item;
+            rowStyle={{ height: 70, padding: 10 }}
+            renderCustomizedRowChild={(selecteditem, index, isSelected) => {
+              return (
+                <View
+                  className={`flex-row items-center h-full border-none outline-none px-3 ${
+                    isSelected && "bg-gray-200"
+                  }`}
+                >
+                  {selecteditem === "Home" ? (
+                    <Entypo name="home" size={20} color="#4a4d4b" />
+                  ) : (
+                    <Icon
+                      name="arrow-top-right-thin-circle-outline"
+                      size={20}
+                      color="#4a4d4b"
+                    />
+                  )}
+                  <Text className="ml-4 font-bold text-gray-600">
+                    {selecteditem}
+                  </Text>
+                </View>
+              );
             }}
+            data={values}
+            onSelect={(selectedItem, index) => {}}
           />
         </View>
 
